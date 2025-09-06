d658ef316d3ccfbb7e0b45e4e395db7fb6c0576d
Azna526 <kaziaznabanu4408@gmail.com>
2025-09-06 15:08:38 +0530

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from movie_loader import load_movies, build_text_model, similar_by_title
 
-# --- Load Data ---
-movies = load_movies()
+st.title("🎬 Dynamic Movie Recommender (TMDb API)")
+
+# --- Load Movies from TMDb ---
+st.info("Fetching movies from TMDb... Please wait.")
+movies = load_movies(pages=3)  # Fetch ~60 movies
 similarity = build_text_model(movies)
 
-# --- UI ---
-st.title("🎬 Movie Recommender with Posters (TMDb API)")
-
 movie_list = movies['title'].values
-selected_movie = st.selectbox("Choose a movie to get recommendations:", movie_list)
+selected_movie = st.selectbox("Choose a movie:", movie_list)
 
 if st.button("Recommend"):
     recommendations = similar_by_title(selected_movie, movies, similarity)
